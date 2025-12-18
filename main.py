e6bb5b29057564fa4b918a1e43722d237bd22333
Simon Orefici Frederiksen <simof@odense.dk>
2025-12-18 12:35:32 +0100

    Proces ready for production - might need optimizing of the process structure

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
-import argparse
 import asyncio
 import logging
 import sys
-import os
 
-from automation_server_client import AutomationServer, Workqueue, WorkItemError, Credential, WorkItemStatus
+from automation_server_client import (
+    AutomationServer,
+    Workqueue,
+    WorkItemError,
+    Credential,
+    WorkItemStatus,
+)
 from datetime import datetime, timedelta, timezone
 from kmd_nexus_client import NexusClientManager
 from odk_tools.tracking import Tracker
-from odk_tools.reporting import report
+from process.nexus_service import NexusService
 
 nexus: NexusClientManager
 tracker: Tracker
-proces_navn = "Afslutning af borgere i genoptræning"
+nexus_service: NexusService
 
 
-async def populate_queue(workqueue: Workqueue):        
+async def populate_queue(workqueue: Workqueue):
     aktivitetsliste = nexus.aktivitetslister.hent_aktivitetsliste(
-        navn="Opgaver: Robot - alle opgavetyper robotten håndterer", 
+        navn="Opgaver: Robot - alle opgavetyper robotten håndterer",
         organisation=None,
         medarbejder=None,
-        antal_sider=10
+        antal_sider=10,
     )
 
     if aktivitetsliste is None:
         return
-    
+
     aktivitetsliste = [
-        aktivitet for aktivitet in aktivitetsliste 
+        aktivitet
+        for aktivitet in aktivitetsliste
         if aktivitet.get("name") == "Robot - afslut borger"
-        and aktivitet["status"] == "Aktiv"        
+        and aktivitet["status"] == "Aktiv"
         and datetime.strptime(aktivitet["date"], "%Y-%m-%dT%H:%M:%S.%f%z")
         > datetime.now(timezone.utc) - timedelta(days=7)
     ]
-    
+
     if aktivitetsliste:
         for aktivitet in aktivitetsliste:
             eksisterende_kødata = workqueue.get_item_by_reference(str(aktivitet["id"]))
@@ -46,16 +51,55 @@ async def populate_queue(workqueue: Workqueue):
 
 async def process_workqueue(workqueue: Workqueue):
     logger = logging.getLogger(__name__)
-
     logger.info("Hello from process workqueue!")
 
     for item in workqueue:
         with item:
             data = item.data  # Item data deserialized from json as dict
- 
+            leverandørnavn = data["description"]
+            fejlbesked = ""
+
             try:
-                # Process the item here
-                pass
+                borger = nexus.borgere.hent_borger(
+                    data["patients"][0]["patientIdentifier"]["identifier"]
+                )
+
+                if borger is None:
+                    continue
+
+                fejlbesked = nexus_service.afslut_indsatser(
+                    borger=borger, leverandørnavn=leverandørnavn
+                )
+
+                if fejlbesked:
+                    # Exit tidligt pga. evt. udenbys borger
+                    nexus_service.afslut_opgave(
+                        borger=borger,
+                        leverandørnavn=leverandørnavn,
+                        opgave_reference=data,
+                        fejl_beskrivelse=fejlbesked,
+                    )
+                    continue
+
+                nexus_service.afslut_skemaer(
+                    borger=borger, leverandørnavn=leverandørnavn
+                )
+
+                nexus_service.fjern_organisationstilknytning(
+                    borger=borger, leverandørnavn=leverandørnavn
+                )
+
+                fejlbesked = nexus_service.kontroller_myndighedsindsatser(
+                    borger=borger, leverandørnavn=leverandørnavn
+                )
+
+                nexus_service.afslut_opgave(
+                    borger=borger,
+                    leverandørnavn=leverandørnavn,
+                    opgave_reference=data,
+                    fejl_beskrivelse=fejlbesked,
+                )
+
             except WorkItemError as e:
                 # A WorkItemError represents a soft error that indicates the item should be passed to manual processing or a business logic fault
                 logger.error(f"Error processing item: {data}. Error: {e}")
@@ -63,25 +107,28 @@ async def process_workqueue(workqueue: Workqueue):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO        
-    )
+    logging.basicConfig(level=logging.INFO)
 
     ats = AutomationServer.from_environment()
     workqueue = ats.workqueue()
 
-    nexus_credential = Credential.get_credential("KMD Nexus - produktion")    
+    nexus_credential = Credential.get_credential("KMD Nexus - produktion")
     tracking_credential = Credential.get_credential("Odense SQL Server")
+
+    tracker = Tracker(
+        username=tracking_credential.username, password=tracking_credential.password
+    )
 
     nexus = NexusClientManager(
         client_id=nexus_credential.username,
         client_secret=nexus_credential.password,
         instance=nexus_credential.data["instance"],
-    )    
-    
+    )
+
+    nexus_service = NexusService(nexus=nexus, tracker=tracker)
+
     tracker = Tracker(
-        username=tracking_credential.username, 
-        password=tracking_credential.password
+        username=tracking_credential.username, password=tracking_credential.password
     )
 
     # Queue management
---
+++ process/nexus_service.py
@@ -0,0 +1,266 @@
+from datetime import datetime, timedelta
+from kmd_nexus_client import NexusClientManager
+from kmd_nexus_client.tree_helpers import filter_by_path, filter_by_predicate
+from odk_tools.tracking import Tracker
+
+
+class NexusService:
+    def __init__(
+        self,
+        nexus: NexusClientManager,
+        tracker: Tracker,
+    ):
+        self.nexus = nexus
+        self.tracker = tracker
+
+    
+    def afslut_indsatser(self, borger: dict, leverandørnavn: str) -> str:
+        pathway = self.nexus.borgere.hent_visning(borger=borger)
+        fejl_besked = ""
+
+        if pathway is None:
+            raise ValueError(
+                f"Kunne ikke finde -Alt for borger {borger['patientIdentifier']['identifier']}"
+            )
+
+        referencer = self.nexus.borgere.hent_referencer(visning=pathway)
+
+        filtrerede_indsats_referencer = filter_by_path(
+            referencer,
+            path_pattern="/Sundhedsfagligt grundforløb/FSIII/Indsatser/Genoptræning udenbys borger (SUL § 140)",
+            active_pathways_only=True,
+        )
+
+        if len(filtrerede_indsats_referencer) > 0:
+            return "Udenbys - Robot"
+
+        filtrerede_indsats_referencer = filter_by_path(
+            referencer,
+            path_pattern="/Sundhedsfagligt grundforløb/FSIII/Indsatser/basketGrantReference",
+            active_pathways_only=True,
+        )
+
+        indsatser_referencer = self.nexus.indsatser.filtrer_indsats_referencer(
+            indsats_referencer=filtrerede_indsats_referencer,
+            kun_aktive=True,
+            leverandør_navn=leverandørnavn,
+        )
+
+        for indsats_reference in indsatser_referencer:
+            indsats = self.nexus.hent_fra_reference(indsats_reference)
+
+            try:
+                if indsats["workflowState"]["name"] == "Tildelt":
+                    self.nexus.indsatser.rediger_indsats(
+                        indsats=indsats, ændringer={}, overgang="Fjern"
+                    )
+                else:
+                    self.nexus.indsatser.rediger_indsats(
+                        indsats=indsats, ændringer={}, overgang="Afslut"
+                    )
+
+            except Exception:
+                return "Slut - Robot"
+
+        return fejl_besked
+
+    def afslut_skemaer(self, borger: dict, leverandørnavn: str):
+        pathway = self.nexus.borgere.hent_visning(borger=borger)
+
+        if pathway is None:
+            raise ValueError(
+                f"Kunne ikke finde -Alt for borger {borger['patientIdentifier']['identifier']}"
+            )
+
+        referencer = self.nexus.borgere.hent_referencer(visning=pathway)
+
+        filtrerede_skema_referencer = filter_by_path(
+            referencer,
+            path_pattern="/Sundhedsfagligt grundforløb/FSIII/formDataV2Reference",
+            active_pathways_only=True,
+        )
+
+        aktive_skemaer = [
+            skema_ref
+            for skema_ref in filtrerede_skema_referencer
+            if skema_ref.get("formDataStatus") == "Aktivt"
+            and skema_ref.get("name") == "Generelle oplysninger"
+        ]
+
+        for skema_reference in aktive_skemaer:
+            skema = self.nexus.hent_fra_reference(skema_reference)
+
+            historik = self.nexus.skemaer.hent_skema_historik(skema=skema)
+            leverandør_audit = sorted(
+                historik, key=lambda entry: entry["version"], reverse=True
+            )
+
+            if len(leverandør_audit) > 0:
+                skema_levarandør = leverandør_audit[0]["professional"][
+                    "primaryOrganization"
+                ]["name"]
+
+                if skema_levarandør == leverandørnavn:
+                    # Fjern skemaets relationer
+                    relationer = self.nexus.nexus_client.get(
+                        skema["_links"]["relatedActivities"]["href"]
+                    ).json()
+
+                    for relation in relationer:
+                        try:
+                            self.nexus.nexus_client.delete(
+                                relation["_links"]["deleteActivityLink"]["href"]
+                            )
+                        except Exception:
+                            continue
+
+                    # Sæt skema til inaktivt
+                    self.nexus.skemaer.rediger_skema(skema, "Inaktivt", data={})
+
+    def fjern_organisationstilknytning(self, borger: dict, leverandørnavn: str):
+        relationer = self.nexus.organisationer.hent_organisationer_for_borger(
+            borger=borger
+        )
+        for relation in relationer:
+            if relation["organization"]["name"] == leverandørnavn:
+                self.nexus.organisationer.fjern_borger_fra_organisation(
+                    organisations_relation=relation
+                )
+
+    def kontroller_myndighedsindsatser(self, borger: dict, leverandørnavn: str) -> str:
+        pathway = self.nexus.borgere.hent_visning(borger=borger)
+
+        if pathway is None:
+            raise ValueError(
+                f"Kunne ikke finde -Alt for borger {borger['patientIdentifier']['identifier']}"
+            )
+
+        referencer = self.nexus.borgere.hent_referencer(visning=pathway)
+
+        filtrerede_indsats_referencer = filter_by_path(
+            referencer,
+            path_pattern="/Sundhedsfagligt grundforløb/FSIII/Indsatser/basketGrantReference",
+            active_pathways_only=False,
+        )
+
+        # Check GGOP
+        ggop_indsatser_referencer = self.nexus.indsatser.filtrer_indsats_referencer(
+            indsats_referencer=filtrerede_indsats_referencer,
+            kun_aktive=True,
+            leverandør_navn="GGOP til anden kommune",
+        )
+
+        if len(ggop_indsatser_referencer) > 0:
+            return "GGOP - Robot"
+
+        # Check andre genoptræningscentre
+        genoptrænings_organisationer = [
+            "Genoptræning Team Nord",
+            "Genoptræning Team Syd",
+            "Genoptræning Team Odense",
+            "Rehabilitering og palliation",
+        ]
+
+        for leverandørnavn in genoptrænings_organisationer:
+            indsatser_referencer = self.nexus.indsatser.filtrer_indsats_referencer(
+                indsats_referencer=filtrerede_indsats_referencer,
+                kun_aktive=True,
+                leverandør_navn=leverandørnavn,
+            )
+
+            if len(indsatser_referencer) > 0:
+                return "Slut - Robot"
+
+        # Check forløbsindplaceringer
+        forløbs_indplacerings_referencer = filter_by_path(
+            referencer,
+            path_pattern="/ÆHF - Forløbsindplacering (Grundforløb)/Forløbsindplacering/Indsatser/basketGrantReference",
+            active_pathways_only=True,
+        )
+
+        aktive_forløbs_indplacerings_referencer = (
+            self.nexus.indsatser.filtrer_indsats_referencer(
+                indsats_referencer=forløbs_indplacerings_referencer,
+                kun_aktive=True,
+            )
+        )
+
+        if len(aktive_forløbs_indplacerings_referencer) > 0:
+            return "Slut - Robot"
+
+        # Check hjælpemidler
+        hjaelpemiddelsindsatser = [
+            "SEL § 86 Træning Hjælpemidler",
+            "SUL § 140 Træning Hjælpemidler",
+            "ÆL § 9 Træning Hjælpemidler",
+        ]
+
+        aktive_udlån = self.nexus.borgere.hent_udlån(borger=borger)
+
+        if aktive_udlån is not None:
+            for udlån in aktive_udlån:
+                grant = udlån.get("grant") or {}
+                if grant.get("name") in hjaelpemiddelsindsatser:
+                    self.nexus.opgaver.opret_opgave(
+                        objekt=udlån,
+                        opgave_type="Opfølgning på udlån af træningsredskab",
+                        titel="Opfølgning på udlån af træningsredskab",
+                        ansvarlig_organisation=leverandørnavn,
+                        start_dato=datetime.now().date(),
+                        forfald_dato=datetime.now().date(),
+                        beskrivelse=""""I forbindelse med afslutning af borgerens træningsforløb, er det konstateret, at der fortsat er aktive udlån af træningsredskaber fra Hjælpemiddelservice. 
+                                        I bedes derfor følge op på disse udlån i forhold til følgende muligheder:
+                                        
+                                        •	Bestil hjemtagning i Nexus (efter forudgående aftale med borger)
+                                        •	Kontakt Myndighed for forespørgsel om overflytning til SEL § 112 – varig bevilling
+                                        •	Bestil hjemtagning af de redskaber, som Myndighed ikke kan bevilge.
+                                        •	Kontakt Hjælpemiddelservice, hvis der er tale om hjælpemidler, der foreslås kasseret (f.eks. ikke genbrugelige kiler)""",
+                    )
+
+        return ""
+
+    def afslut_opgave(
+        self,
+        borger: dict,
+        leverandørnavn: str,
+        opgave_reference: dict,
+        fejl_beskrivelse: str,
+    ):
+        if fejl_beskrivelse:
+            pathway = self.nexus.borgere.hent_visning(borger=borger)
+
+            if pathway is None:
+                raise ValueError(
+                    f"Kunne ikke finde -Alt for borger {borger['patientIdentifier']['identifier']}"
+                )
+
+            referencer = self.nexus.borgere.hent_referencer(visning=pathway)
+
+            skema_referencer = filter_by_path(
+                referencer,
+                path_pattern="/Sundhedsfagligt grundforløb/FSIII/formDataV2Reference",
+                active_pathways_only=True,
+            )
+
+            slutnotat_referencer = [
+                skema_ref
+                for skema_ref in skema_referencer
+                if skema_ref.get("name") == "Slutnotat træning"
+            ]
+            slutnotat_referencer.sort(key=lambda x: x.get("date", ""), reverse=True)
+
+            if len(slutnotat_referencer) > 0:
+                slutnotat = self.nexus.hent_fra_reference(slutnotat_referencer[0])
+                self.nexus.opgaver.opret_opgave(
+                    objekt=slutnotat,
+                    opgave_type="Tværfagligt samarbejde",
+                    titel=fejl_beskrivelse,
+                    ansvarlig_organisation="Myndighed genoptræning",
+                    start_dato=datetime.now().date(),
+                    forfald_dato=datetime.now().date() + timedelta(days=7),
+                    beskrivelse=f"""Opgave oprettet på vegne af: {leverandørnavn}""",
+                )
+
+        opgave = self.nexus.hent_fra_reference(opgave_reference)
+        self.nexus.opgaver.luk_opgave(opgave=opgave)
+        self.tracker.track_task("Afslutning af borgere i genoptræning")
